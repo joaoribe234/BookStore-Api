510bc7a2ff943266b9d563a0d900dabc6626a803
joaoribe234 <joaoribe234@gmail.com>
2022-08-31 09:51:47 -0300

    Repositories Implementadas

---
+++ src/main/java/unimed/test/BookStore/Repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package unimed.test.BookStore.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import unimed.test.BookStore.domain.Categoria;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Categoria, Integer> {
+
+}
